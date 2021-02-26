3e3770a5fc0a5565f3395cf3f82d9781d38c81f4
Saúl Camacho <electrohead1200@gmail.com>
2021-02-26 00:39:20 -0600

    Creando Menú Responsive (Hamburguesa), HTML, CSS, Java Script

---
+++ src/js/app.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+    eventListeners();
+});
+
+function eventListeners() {
+    const mobileMenu = document.querySelector('.mobile-menu');
+
+    mobileMenu.addEventListener('click', navegacionResponsive);
+}
+
+function navegacionResponsive() {
+    const navegacion = document.querySelector('.navegacion');
+
+    if(navegacion.classList.contains('mostrar')) {
+        navegacion.classList.remove('mostrar');
+    } else {
+        navegacion.classList.add('mostrar');  
+    } 
+    // Se puede sustituir con TOGGLE: navegacion.classList.toggle('mostrar');
+}
