f10ef91af5a0298642e3ca3b36f28f88bcc65c48
Saúl Camacho <electrohead1200@gmail.com>
2021-03-05 11:03:55 -0600

    Creando una Tabla par listar las propiedades, y conectandola con la Base de Datos

--- admin/index.php
+++ admin/index.php
@@ -1,5 +1,17 @@
 <?php 
 
+    //Importar la conexion
+    require './../includes/config/database.php';
+    $db = conectarDB();
+
+    // Escribir el Query SQL
+    $query = "SELECT * FROM propiedades";
+
+    // Muestra mensaje condicional
+
+    $resultadoConsulta = mysqli_query($db, $query);
+
+    // Incluye un Template
     $resultado = $_GET['resultado'] ?? null;
     require '../includes/funciones.php';
     incluirTemplate('header');
@@ -12,8 +24,40 @@
         <?php endif; ?>
 
         <a href="/admin/propiedades/crear.php" class="boton-verde">Nueva Propiedad</a>
+
+        <table class="propiedades">
+            <thead>
+                <tr>
+                <th>ID</th>
+                <th>Titulo</th>
+                <th>Imagen</th>
+                <th>Precio</th>
+                <th>Acciones</th>
+                </tr>
+            </thead>
+
+            <tbody> <!-- Mostrar los resultados -->
+            <?php while( $propiedad = mysqli_fetch_assoc($resultadoConsulta)): ?>
+                <tr>
+                    <td><?php echo $propiedad['id']; ?></td>
+                    <td><?php echo $propiedad['titulo']; ?></td>
+                    <td><img src="/imagenes/<?php echo $propiedad['imagen']; ?>" alt="Imagen Tabla" class="imagen-tabla"></td>
+                    <td>$ <?php echo $propiedad['precio']; ?></td>
+                    <td>
+                     <a href="#" class="boton-rojo-block">Eliminar</a>
+                    <a href="#" class="boton-amarillo-block">Actualizar</a>
+                    </td>
+                </tr>
+                <?php endwhile; ?>
+            </tbody>
+        </table>
+
+
     </main>
 
     <?php // La funcion ya fue incluida en el header
+
+    mysqli_close($db); // Cerrar la conexion a la Base de Datos
+
     incluirTemplate('footer');
     ?>
